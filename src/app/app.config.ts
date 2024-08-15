12608dcdf9a9d5e44eb404d7091811aaa778d5c9
unknown <santiscally@gmail.com>
2024-08-15 20:29:12 -0300

    cambio de git

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,20 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideToastr } from 'ngx-toastr';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWgcHOdU3Yja7DM8kthEIM-q5mlQmAtDA",
+  authDomain: "los-verdes-36c62.firebaseapp.com",
+  projectId: "los-verdes-36c62",
+  storageBucket: "los-verdes-36c62.appspot.com",
+  messagingSenderId: "554381483252",
+  appId: "1:554381483252:web:96f9b8a0d73ad320eb92a1"
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideFirebaseApp(() => initializeApp({"projectId":"los-verdes-ab4ad","appId":"1:487765646559:web:10ad810c003ee31e42dd6a","storageBucket":"los-verdes-ab4ad.appspot.com","apiKey":"AIzaSyARDrgr-cCQe_g0gVqAOEwJRsS3Ik9u-mk","authDomain":"los-verdes-ab4ad.firebaseapp.com","messagingSenderId":"487765646559"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"los-verdes-ab4ad","appId":"1:487765646559:web:10ad810c003ee31e42dd6a","storageBucket":"los-verdes-ab4ad.appspot.com","apiKey":"AIzaSyARDrgr-cCQe_g0gVqAOEwJRsS3Ik9u-mk","authDomain":"los-verdes-ab4ad.firebaseapp.com","messagingSenderId":"487765646559"})), 
+    provideFirebaseApp(() => initializeApp(firebaseConfig)), 
     provideAuth(() => getAuth()), 
     provideFirestore(() => getFirestore()),
     provideAnimations(), // required animations providers
