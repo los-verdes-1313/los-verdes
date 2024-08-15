289845d685dfe4478327b687f9b64c8862ec86f5
unknown <santiscally@gmail.com>
2024-08-14 22:45:14 -0300

    se hizo responsive el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, HostListener, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -8,14 +8,27 @@ import { AuthService } from '../../services/auth.service';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
-  @ViewChild('dropdownToggle', { static: true }) dropdownToggle!: ElementRef;
+  isSidebarOpen = false;
   isDropdownOpen = false;
 
+  @ViewChild('dropdownToggle') dropdownToggle!: ElementRef;
+
+  navItems = [
+    { route: '/create-order', label: 'Crear Pedidos' },
+    { route: '/orders', label: 'Órdenes de Compra' },
+    { route: '/add-product', label: 'Productos' },
+    { route: '/clients', label: 'Clientes' }
+  ];
+
   constructor(private authService: AuthService, private router: Router) {}
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    document.body.style.overflow = this.isSidebarOpen ? 'hidden' : '';
+  }
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
@@ -25,6 +38,13 @@ export class HeaderComponent {
   onDocumentClick(event: MouseEvent) {
     if (this.dropdownToggle && !this.dropdownToggle.nativeElement.contains(event.target)) {
       this.isDropdownOpen = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth >= 992 && this.isSidebarOpen) {
+      this.toggleSidebar();
     }
   }
 
