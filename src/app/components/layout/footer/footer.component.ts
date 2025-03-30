e6b8fb0ec2aec214299b979548dbf0b98a3b40e0
santiscally <santiscally@gmail.com>
2025-03-29 23:02:03 -0300

    cambios en el footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -17,22 +17,22 @@ interface SocialLink {
 export class FooterComponent {
   socialLinks: SocialLink[] = [
     {
-      name: 'GitHub',
-      iconDesktop: 'RRSS=Github, Dispositivo=Desktop.png',
-      iconMobile: 'RRSS=Github, Dispositivo=Mobile.png',
-      url: 'https://github.com/santiscally?tab=repositories'
+      name: 'Whatsapp',
+      iconDesktop: 'RRSS=Whatsapp, Dispositivo=Desktop.png',
+      iconMobile: 'RRSS=Whatsapp, Dispositivo=Mobile.png',
+      url: 'https://wa.me/5491123992362?text=Hola,%20me%20gustaría%20coordinar%20una%20entrevista%20para%20conocer%20más%20sobre%20sus%20servicios.'
     },
     {
       name: 'Email',
       iconDesktop: 'RRSS=Email, Dispositivo=Desktop.png',
       iconMobile: 'RRSS=Email, Dispositivo=Mobile.png',
-      url: 'mailto:santiscally@gmail.com?subject=Consulta%20Simple%20Apps&body=Hola,%20me%20gustaría%20obtener%20más%20información%20sobre%20sus%20servicios.'
+      url: 'mailto:contacto.simpleapps@gmail.com?subject=Consulta%20Simple%20Apps&body=Hola,%20me%20gustaría%20obtener%20más%20información%20sobre%20sus%20servicios.'
     },
     {
-      name: 'Whatsapp',
-      iconDesktop: 'RRSS=Whatsapp, Dispositivo=Desktop.png',
-      iconMobile: 'RRSS=Whatsapp, Dispositivo=Mobile.png',
-      url: 'https://wa.me/5491123992362?text=Hola,%20me%20gustaría%20coordinar%20una%20entrevista%20para%20conocer%20más%20sobre%20sus%20servicios.'
+      name: 'Instagram',
+      iconDesktop: 'RRSS=Instagram, Dispositivo=Desktop.png',
+      iconMobile: 'RRSS=Instagram, Dispositivo=Mobile.png',
+      url: 'https://www.instagram.com/simpleapps.com.ar/?utm_source=ig_web_button_share_sheet'
     },
     {
       name: 'LinkedIn',
